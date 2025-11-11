338aa6092a58cf5021e39fd52b4c06137e6efe0b
Admin <Admin@CAB103-5>
2025-11-11 14:36:16 +1000

    Добавьте файлы проекта.

---
+++ Addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coffee
+{
+    public class Addition
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Coffee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coffee
+{
+    public class Coffee
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public List<Addition> Additions { get; set; } = new List<Addition>();
+
+    }
+}
